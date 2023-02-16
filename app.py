2c9d6223ce29e24217b43e3c8bf258e96cdae77e
ppbit-lp <ppbit_lp@msn.com>
2023-02-16 13:10:10 +0800

    modify chatbot module

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
 from flask import Flask, render_template, request
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
 
 app = Flask(__name__)
+class ENGSM:
+    ISO_639_1 = 'en_core_web_sm'
+english_bot = ChatBot("Chatterbot", 
+    storage_adapter="chatterbot.storage.SQLStorageAdapter",
+    tagger_language = ENGSM)
+conversation = [
+    "Hello",
+    "Hi there!",
+    "How are you doing?",
+    "I'm doing great.",
+    "That is good to hear",
+    "Thank you.",
+    "You're welcome.",
+    "Never gonna give you up",
+    "Never gonna let you down"
+]
+trainer = ListTrainer(english_bot)
+trainer.train(conversation)
 
-english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
-trainer = ChatterBotCorpusTrainer(english_bot)
-trainer.train("chatterbot.corpus.english")
 
 @app.route("/")
 def home():
